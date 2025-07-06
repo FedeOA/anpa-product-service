79ae5af8fd63edc1d17c512b27c3a5b9722e0efb
Ignacio Sanchez <sanchezih@gmail.com>
2025-07-06 11:37:18 -0300

    upadte

---
+++ src/main/java/com/github/sanchezih/product/AnpaProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.sanchezih.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnpaProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnpaProductServiceApplication.class, args);
+	}
+
+}
